44bdbb105ff8b040dc8ba1ff530d9ffbeae04469
hanywyam <hellow522@naver.com>
2023-11-24 15:14:54 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Main from "./AppPage/Main";
+
+function App() {
+  return (
+    <>
+      {/* <h1>리액트</h1> */}
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/AppPage/MainTop.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaBeer } from "react-icons/fa";
+import { IoArrowUpCircle } from "react-icons/io5";
+import topTgg from "./style/mainTopTgg.module.css";
+
+// TOP 올라가는 버튼
+const MainTop = () => {
+  return (
+    <div>
+      <a href="#" className={topTgg.topTgg}>
+        <IoArrowUpCircle style={{ width: 100, height: 100 }} />
+      </a>
+    </div>
+  );
+};
+
+export default MainTop;
